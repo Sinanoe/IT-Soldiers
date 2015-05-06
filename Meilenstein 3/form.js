cb334f832233a686818641f85ca061bea84ae06b
Hoefli93 <hoefli93@web.de>
2015-05-06 15:06:09 +0200

    Added: book_style.css M3 A1 Changed: book.html, form.js -t.höfler

--- Meilenstein 3/form.js
+++ Meilenstein 3/form.js
@@ -1,66 +1,110 @@
 function check(form, vorname, nachname, buchautor, buchtitel, isbn, erscheinungsjahr, auflage) {
   
 	recheck = /^[\sa-zßA-ZäöüÄÖÜ]+$/;
-	// Überprüfe eingegebenen Vorname auf Gültigkeit
-	if(!recheck.test(form.vorname.value)) {
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-		form.vorname.focus();
-		return false;
-	}
-	// Überprüfe eingegebenen Nachname auf Gültigkeit
-	if(!recheck.test(form.nachname.value)) {
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-		form.nachname.focus();
-		return false;
-	}
-	// Überprüfe eingegebenen Buchautor auf Gültigkeit
-	if(!recheck.test(form.buchautor.value)) {
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
-		form.buchautor.focus();
-		return false;
-	}
 	// Überprüfe eingegebenen Buchtitel auf Gültigkeit
 	if(form.buchtitel.value == '') {
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		form.buchtitel.style.border = "1px solid #FF0000";
 		form.buchtitel.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
 		return false;
 	
 	}
 
-	recheck = /^(97(8|9))?\d{9}(\d|X)$/;
+	else {
+		form.buchtitel.style.border = "";
+		
+
+	}
+	
+
+	
+	// Überprüfe eingegebenen Buchautor auf Gültigkeit
+	if(!recheck.test(form.buchautor.value)) {
+		form.buchautor.style.border = "1px solid #FF0000";
+		form.buchautor.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		return false;
+	}
+
+	else {
+		form.buchautor.style.border = "";
+		
+
+	}
+	
+	recheck = /^\d{12}(\d|X)$/;
 	
 	if(!recheck.test(form.isbn.value)) {
-	
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		form.isbn.style.border = "1px solid #FF0000";
 		form.isbn.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
 		return false;
 		
 	}
 
+	else {
+		form.isbn.style.border = "";
+		
+
+	}
 	recheck = /^[0-9]+$/;
 
 	if(!recheck.test(form.erscheinungsjahr.value) || form.erscheinungsjahr.value < 1000 || form.erscheinungsjahr.value >= 2015) {
-	
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		form.erscheinungsjahr.style.border = "1px solid #FF0000";
 		form.erscheinungsjahr.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
 		return false;
 		
 	}
 
-	
+	else {
+		form.erscheinungsjahr.style.border = "";
+		
+
+	}	
 	recheck = /^[0-9]+$/;
 
 	if(!recheck.test(form.auflage.value)) {
-	
-		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		form.auflage.style.border = "1px solid #FF0000";
 		form.auflage.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
 		return false;
 		
 	}
-	
-	
-	
 
+	else {
+		form.auflage.style.border = "";
+		
+
+	}
+	recheck = /^[\sa-zßA-ZäöüÄÖÜ]+$/;
+	// Überprüfe eingegebenen Vorname auf Gültigkeit
+	if(!recheck.test(form.vorname.value)) {
+		form.vorname.style.border = "1px solid #FF0000";
+		form.vorname.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		return false;
+	}
+
+	else {
+		form.vorname.style.border = "";
+		
+
+	}
+	
+	// Überprüfe eingegebenen Nachname auf Gültigkeit
+	if(!recheck.test(form.nachname.value)) {
+		form.nachname.style.border = "1px solid #FF0000";
+		form.nachname.focus();
+		alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+		return false;
+	}
+
+	else {
+		form.nachname.style.border = "";
+		
+
+	}
 	
 
 
