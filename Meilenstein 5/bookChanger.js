da99f6fabd7ccf9a0f4bbf85e73726aa31b9a83a
Höfli <Hfli@192.168.2.104>
2015-06-16 17:05:46 +0200

    Changed: bookChanger.js, book.html added function load -t.höfler

--- Meilenstein 5/bookChanger.js
+++ Meilenstein 5/bookChanger.js
@@ -1,8 +1,7 @@
 /**
- * Tabelle mitHorrorbuechern ein.
- * Tabelle mitRomanbuechern  aus.
- * Farbe des Horror-Tabs wird auf #3F48CC gesetzt.
- * Farbe des Roman-Tabs wird auf #00A2E8 gesetzt.
+ * Horrorbücher ein 
+ * Farbänderung des Horror-Tabs auf #3F48CC
+ * Farbänderung des Roman-Tabs auf #00A2E8
  */
 function tab_horror_switcher() {
    
@@ -10,17 +9,19 @@ function tab_horror_switcher() {
 	document.getElementById('horror').style.backgroundColor="#3F48CC";
 	document.getElementById('roman').style.backgroundColor="#00A2E8";
 }
-
 /**
- * Tabelle mit Romanbuechern ein.
- * Tabelle mit Horrorbuechern aus.
- * Farbe wird auf Standardwert #3F48CC gesetzt.
- * Farbe des Horror-Tabs wird auf #00A2E8 gesetzt.
+ * Romanbücher ein
+ * Farbänderung des Horror-Tabs auf #3F48CC
+ * Farbänderung des Roman-Tabs auf #00A2E8
  */
 function tab_roman_switcher() {
  
 	document.getElementById('horror').style.backgroundColor="#00A2E8";
 	document.getElementById('roman').style.backgroundColor="#3F48CC";
+}
+
+function load() {
+		showJSON('horror');
 }
 
 /**
@@ -36,23 +37,26 @@ function createTable(genre) {
 	} 
 	if (window.XMLHttpRequest) {
 		ajax=new XMLHttpRequest();
-	} else {  // old browser compatibility (IE5, IE6)
+	} else {  
 		ajax=new ActiveXObject("Microsoft.XMLHTTP");
+		//  Browser Kompatibilität 
 	}
 	ajax.onreadystatechange=function() {
 		if (ajax.readyState==4 && ajax.status==200) {
 			jsonString=ajax.responseText;
-			jsonObj = JSON.parse(jsonString);
+			jsonHTML = "<table class='bookInventory'><tr><th>Autor</th><th>Titel</th><th>Kapitel</th><th>Art des Buches</th><th>ISBN</th><th>Erscheinungsjahr</th><th>Auflage</th></tr>";
+			if (jsonString != '' && jsonString.search("titel") != -1) {
+				jsonObj = JSON.parse(jsonString);
 			
-			jsonHTMLFormat = "<table class='bookInventory'><tr><th>Autor</th><th>Titel</th><th>Kapitel</th><th>Art des Buches</th><th>ISBN</th><th>Erscheinungsjahr</th><th>Auflage</th></tr>";
-			for(i=0;i<jsonObj.bookdata.length;i++) {
-				jsonHTMLFormat=jsonHTMLFormat + "<tr><td>"+ jsonObj.bookdata[i].autor+"</td><td>"+ jsonObj.bookdata[i].titel+"</td><td>"+ jsonObj.bookdata[i].kapitel+"</td><td>"+ jsonObj.bookdata[i].buchart+"</td><td>"+ jsonObj.bookdata[i].ISBN+"</td><td>"+ jsonObj.bookdata[i].erscheinungsjahr+"</td><td>"+ jsonObj.bookdata[i].auflage+"</td></tr>";
-
-			}
-			jsonHTMLFormat=jsonHTMLFormat + "</table>";
-			document.getElementById("bookInventar").innerHTML=jsonHTMLFormat;
-		
-
+				for(i=0;i<jsonObj.bookdata.length;i++) {
+					jsonHTML=jsonHTML + "<tr><td>" + jsonObj.bookdata[i].autor + "</td><td>" + jsonObj.bookdata[i].titel + "</td><td>" + jsonObj.bookdata[i].kapitel + "</td><td>" + jsonObj.bookdata[i].buchart + "</td><td>" + jsonObj.bookdata[i].isbn + "</td><td>" + jsonObj.bookdata[i].erscheinungsjahr + "</td><td>" + jsonObj.bookdata[i].auflage + "</td></tr>";
+				}
+			} else {
+				jsonHTML=jsonHTML + "<tr><td colspan='7'>Zu dieser Genre sind keine B&uuml;cher vorhanden oder die Genre ist ungültig.</td></tr>";
+			} 
+			
+			jsonHTML=jsonHTML + "</table>";
+			document.getElementById("bookInventar").innerHTML=jsonHTML;
 
 		}
 	}
