96a1dd58cc950bc19927b7bcef11aab8354b2604
Höfli <Hfli@192.168.2.104>
2015-06-20 20:03:09 +0200

    Changed: M5 getBooks.php - t.höfler

--- Meilenstein 5/getBooks.php
+++ Meilenstein 5/getBooks.php
@@ -1,19 +1,25 @@
 <?php
-// JSON Definition
+
+ // Passende JSON-Datei wird gestellt und an den Client zurückgesendet
+
+// Definition JSON 
+
 header('Content-Type: application/json');
+
 // json Request
-$json=$_GET["json"];
-// Pfad zur JSON Objekt-Datei
-$file = 'scripts/json/'.$json.'_books.json';
-// Daten werden ausgewertet und JSON-Datei wird ausgegeben. Im Fehlerfalle Fehlermeldung.
-if (file_exists($file)) {
-	$json_obj = file_get_contents($file);
-	$json_obj = utf8_encode($json_obj); 
-	echo $json_obj;
-} else {
-	echo("<table class='bookInventory'><tr>");
-	echo ("<tr><td>Es ist irgendetwas schief gelaufen. Falls das Problem noch einmal auftreten sollte, schreiben Sie uns eine E-Mail!</td></tr>");
-	echo ("<tr><td><b>info@nixgehtmehr.de</b></td></tr>");
-	echo("</table>");
+if (isset ( $_GET["json"] )) {
+	$json=$_GET["json"];
+	
+	// Pfad JSON Objekt-Datei
+	$filename = 'scripts/json/'.$json.'_books.json';
+	
+	// Daten werden ausgewertet und JSON-Datei wird ausgegeben. Im Fehlerfalle wird ein leerer String ausgeworfen
+	if (file_exists($filename)) {
+		$json_obj = file_get_contents($filename);
+		$json_obj = utf8_encode($json_obj); 
+		echo $json_obj;
+	} else {
+		echo("");
+	}
 }
 ?>
